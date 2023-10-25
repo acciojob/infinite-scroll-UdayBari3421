0a130ae61b004987f6b2b1c69f027054fb09a5ff
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-25 12:13:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let ol = document.getElementById("infi-list");
 
-let scrollHeit = ol.scrollHeight;
+let scrollHeit = 1;
 const loop = () => {
   for (let i = 1; i <= 10; i++) {
     let created = document.createElement("li");
-    created.innerText = `${"List Item "}` + i;
+    created.innerText = `${"List Item " + scrollHeit ++}`;
     ol.appendChild(created);
   }
 };
 ol.addEventListener("scroll", (e) => {
-  if (ol.clientHeight <= ol.scrollHeight) {
+  let totalScrollHeight = ol.scrollHeight;
+  let visible = ol.clientHeight;
+  let scrollHeight = ol.scrollTop;
+  let rem = totalScrollHeight - visible - scrollHeight;
+  
+  if (rem <= 0) {
     loop();
   }
 });
